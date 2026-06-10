33bb09c3307c5cd96bb5d6abbb085840e8b497fd
Gabs-bertolini <gabrielbertolinibarioni@gmail.com>
2026-06-10 19:28:52 -0300

    captura inputs.

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 // Arquivo para código javascript
+const API = "https://6a1c77bf8858a003817be931.mockapi.io/api/v1/"
+const formulario = document.getElementById("formulario")
+const inputNome = document.getElementById("input-nome")
+const inputQtd = document.getElementById("input-quantidade")
+const lista = document.getElementById("lista-materiais")
+const botao = document.getElementById("btn-cadastrar")
+
