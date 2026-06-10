4e043326a789b349fad1681b9a9a46b93777b7e5
Gabs-bertolini <gabrielbertolinibarioni@gmail.com>
2026-06-10 19:53:39 -0300

    adiciona API e leitura de objeto.

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 // Arquivo para código javascript
-const API = "https://6a1c77bf8858a003817be931.mockapi.io/api/v1/"
+const API_URL = "https://6a1c77bf8858a003817be931.mockapi.io/api/v1/Material"
 const formulario = document.getElementById("formulario")
-const inputNome = document.getElementById("input-nome")
-const inputQtd = document.getElementById("input-quantidade")
 const lista = document.getElementById("lista-materiais")
-const botao = document.getElementById("btn-cadastrar")
 
+const buscarProdutos = async () => {
+  const resposta = await fetch(API_URL);
+  const dados = await resposta.json();
+
+  lista.innerHTML = "";
+
+  dados.forEach((produto) => {
+    const linha = document.createElement("tr");
+
+    linha.innerHTML = `
+      <td>${produto.PRODUTO}</td> 
+      <td>${produto.QUANTIDADE}</td>
+    `;
+    lista.appendChild(linha);
+  });
+};
