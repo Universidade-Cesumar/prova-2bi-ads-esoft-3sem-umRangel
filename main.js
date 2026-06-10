a5aacd40f15b8e0fbb8255e19e71689616d1faf4
Gabs-bertolini <gabrielbertolinibarioni@gmail.com>
2026-06-10 20:18:42 -0300

    ajusta as config do API e Função.

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // Arquivo para código javascript
 const API_URL = "https://6a1c77bf8858a003817be931.mockapi.io/api/v1/Material"
 const formulario = document.getElementById("formulario")
-const lista = document.getElementById("lista-materiais")
+const tabela = document.getElementById("lista-materiais")
 
 const buscarProdutos = async () => {
   const resposta = await fetch(API_URL);
   const dados = await resposta.json();
 
-  lista.innerHTML = "";
+  tabela.innerHTML = "";
 
   dados.forEach((produto) => {
     const linha = document.createElement("tr");
@@ -16,25 +16,27 @@ const buscarProdutos = async () => {
       <td>${produto.MATERIAL}</td> 
       <td>${produto.QUANTIDADE}</td>
     `;
-    lista.appendChild(linha);
+    tabela.appendChild(linha);
   });
 };
+
+buscarProdutos();
 
 formulario.addEventListener("submit", async (evento) =>{
     evento.preventDefault
 
     const novoMaterial = {
-        MATERIAL: document.getElementById("input-nome").value
+        MATERIAL: document.getElementById("input-nome").value,
         QUANTIDADE: Number(document.getElementById("input-quantidade").value)
     }
 
     const resposta = await fetch(API_URL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/JSON"
-        }
-        body: JSON.stringify("novoMaterial")
-    })
+    method: "POST",
+    headers: {
+      "Content-Type": "Application/json"
+    },
+    body: JSON.stringify(novoMaterial)
+  });
 
     if(resposta.ok){
         formulario.reset()
